28c9833918e537986292fabd8c21fbc2a44a6f63
SheharyarZahid <sheharyarz1510@gmail.com>
2023-10-05 20:36:55 +0500

    Week5

---
+++ PF lab/week5/task3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+
+main(){
+    float number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    float squareRoot;
+    squareRoot = sqrt(number);
+
+    cout << "The square root of " << number << " is: " << squareRoot; 
+
+}
