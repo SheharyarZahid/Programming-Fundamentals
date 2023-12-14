00609570a2e59d34b44607299817d3b44cfbbc0b
SheharyarZahid <sheharyarz1510@gmail.com>
2023-12-14 19:04:00 +0500

    Projects

--- game project/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-void initiateBullet(char[][156]);
-void moveBullet(char[][156]);
-void eraseBullet(char[][156]);
-void moveOpponent(char[][156], char, char);
-bool checkWallCollision(char[][156]);
-void moveOpponent(char [][156], char, char);
-int getRandomNumber(int min, int max);
